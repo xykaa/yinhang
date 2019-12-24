ffb29baf1e696a32fe89d8be028f1cfaa9bc416c
jk <784383619qq.com>
2019-12-24 10:54:56 +0800

    运行生成

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => 'localhost',
     // 数据库名
-    'database'        => 'shop_new_4_26',
+    'database'        => 'yinhang123',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => '123',
 
     // 端口
     'hostport'        => '3306',
